64fbef3ff873e047e13eaddbb370a3d6209d95c4
Lucas Guimarães Bernardes <skypartan@gmail.com>
2018-06-22 11:46:20 -0300

    Página de busca

---
+++ Source/scripts/util.js
@@ -0,0 +1,9 @@
+
+function pAvancadaShow() {
+    let pAvancada = document.getElementById('pAvancada');
+
+    if (pAvancada.style.display == 'none')
+        pAvancada.style.display = '';
+    else
+        pAvancada.style.display = 'none';
+}
